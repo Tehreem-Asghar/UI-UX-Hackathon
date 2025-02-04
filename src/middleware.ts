a64ec92a4683b0ee8da3d601ba2d1092fd4492ab
Tehreem-Asghar <arhammeo49@gmail.com>
2025-02-04 21:53:45 +0500

    update sanity api token

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,44 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 export const middleware = (request: NextRequest) => {
+
+  try {
     const cookie = request.cookies.get("next-auth.session-token")?.value;
     const deployedCookie = request.cookies.get("__Secure-next-auth.session-token")?.value;
   
-    // Log cookie values for debugging
     console.log("Cookie:", cookie);
     console.log("DeployedCookie:", deployedCookie);
   
-    // Check if both cookies exist
     if (!cookie && !deployedCookie) {
       const loginUrl = new URL("/login", request.url);
       return NextResponse.redirect(loginUrl);
     }
-  
     return NextResponse.next();
+} catch (error) {
+    console.error("Middleware Error:", error);
+    return NextResponse.next();
+}
+
+
+
+
+
+
+
+    // const cookie = request.cookies.get("next-auth.session-token")?.value;
+    // const deployedCookie = request.cookies.get("__Secure-next-auth.session-token")?.value;
+  
+    // // Log cookie values for debugging
+    // console.log("Cookie:", cookie);
+    // console.log("DeployedCookie:", deployedCookie);
+  
+    // // Check if both cookies exist
+    // if (!cookie && !deployedCookie) {
+    //   const loginUrl = new URL("/login", request.url);
+    //   return NextResponse.redirect(loginUrl);
+    // }
+  
+    // return NextResponse.next();
   };
   
   export const config = {
